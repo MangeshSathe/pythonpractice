8f8836c27fd64eb48e30dbea5f5717cac6c5a4e6
Mangesh Sathe <mangeshsathe21@gmail.com>
2025-08-07 13:31:26 +0530

    calculate hora based on sunrise and sunset vaidik astrology

---
+++ constants.py
@@ -0,0 +1,2 @@
+DAY = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+PLANET_HORA = ['Sun','Moon','Mars','Mercury','Jupiter','Venus','Saturn']
--- hora.py
+++ hora.py
@@ -1,28 +1,59 @@
 """
 hora calculation
 """
-
-
 from datetime import datetime,timedelta
-day = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-planet_hora = ['Sun','Moon','Mars','Mercury','Jupiter','Venus','Saturn']
-counter = 1 
-day_night = 'Day'
+import math
+import pdb
+import constants
 
-sunrise_current_day = datetime(2025,8,10,6,15,55)
+class Hora:
+    def __init__(self):
+        pass
+
+    def list_hora(self, sunrise_current_day, sunrise_next_day):
+        hora_start = sunrise_current_day 
+        total_duration = ( sunrise_next_day - sunrise_current_day ) / 24
+        total_duration_sec =  total_duration.total_seconds()
+        day_of_the_week = int(sunrise_current_day.strftime("%w"))
+
+        #pdb.set_trace()
+
+        while hora_start  < sunrise_next_day :
+            from_timestamp = hora_start.replace(microsecond= 0)
+            to_timestamp = (hora_start + timedelta(seconds = total_duration_sec)).replace(microsecond=0)
+            print(f'Hora of {constants.PLANET_HORA[day_of_the_week]}:  From {from_timestamp} to {to_timestamp}')
+            hora_start = hora_start + timedelta(seconds = total_duration_sec)
+            day_of_the_week+=5
+            if day_of_the_week > 6:
+                day_of_the_week = day_of_the_week - 7
+
+    def get_hora_of_planet(self, sunrise_current_day, sunrise_next_day, hora_time):
+        counter = 1 
+        hora_start = sunrise_current_day 
+        total_duration = ( sunrise_next_day - sunrise_current_day ) / 24
+        total_duration_sec =  total_duration.total_seconds()
+        day_of_the_week = int(sunrise_current_day.strftime("%w"))
+
+        while hora_start  < sunrise_next_day :
+            from_timestamp = hora_start.replace(microsecond= 0)
+            to_timestamp = (hora_start + timedelta(seconds = total_duration_sec)).replace(microsecond=0)
+
+            if from_timestamp <= hora_time <= to_timestamp:
+                return constants.PLANET_HORA[day_of_the_week]
+            hora_start = hora_start + timedelta(seconds = total_duration_sec)
+            day_of_the_week+=5
+            if day_of_the_week > 6:
+                day_of_the_week = day_of_the_week - 7
+
+sunrise_current_day = datetime(2025,8,10,6,14,55)
 sunrise_next_day = datetime(2025,8,11,6,15,11)
-hora_start = sunrise_current_day 
-total_duration = ( sunrise_next_day - sunrise_current_day ) / 24
-total_duration_sec =  total_duration.total_seconds()
+find_hora_in = datetime(2025,8,10,22,30,11)
 
-day_of_the_week = int(sunrise_current_day.strftime("%w"))
+hora_listing = Hora()
 
-while hora_start < sunrise_next_day:
-    if counter > 12:
-        day_night = 'Night'
-    print(f'{day_night} hora of {planet_hora[day_of_the_week]}:  From {hora_start} to {hora_start + timedelta(seconds = total_duration_sec)}')
-    hora_start = hora_start + timedelta(seconds = total_duration_sec)
-    counter+=1
-    day_of_the_week+=5
-    if day_of_the_week > 6:
-        day_of_the_week = day_of_the_week - 7
+hora_listing.list_hora(sunrise_current_day, sunrise_next_day)
+planet = hora_listing.get_hora_of_planet(sunrise_current_day, sunrise_next_day,find_hora_in)
+if planet != "":
+    print(f'\n\n\n\n {find_hora_in }Belongs to the {planet}')
+else:
+    print(f'unable to find! please check time')
